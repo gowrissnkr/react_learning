6ae73a87aa9f62f4d4de77a82a6cff289b1775e3
gowrissnkr <98967358+gowrissnkr@users.noreply.github.com>
2023-09-05 06:16:37 +0530

    Episode-3

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
 
-const heading = React.createElement("div", { id: "parent" },
-    [React.createElement("div", { id: "child" },
-        [React.createElement("h1", { id: "" }, "Im H1 Tag"),
-        React.createElement("h2", { id: "" }, "Im H2 Tag")]
-    ),
-    React.createElement("div", { id: "child" },
-        [React.createElement("h1", { id: "" }, "Im H1 Tag"),
-        React.createElement("h2", { id: "" }, "Im H2 Tag")]
+const Header = () => {
+    return (
+        <nav id="main-nav" className="tranformClass">
+            <div className="left-menu">
+                <div className="nav-logo">
+                    <a href=""> <img src="https://static.wixstatic.com/media/84b06e_717d310324e54451a0195d5658c70004~mv2.png/v1/fill/w_640,h_366,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/84b06e_717d310324e54451a0195d5658c70004~mv2.png" alt="logo" /></a>
+                </div>
+            </div>
+            <div className="mid-menu">
+                <input type="text" placeholder="Search..." />
+            </div>
+            <div className="right-menu">
+                <img src="https://png.pngtree.com/element_our/20200610/ourmid/pngtree-character-default-avatar-image_2237203.jpg" />
+            </div>
+        </nav>
     )
-    ]
-);
+}
 
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading)
+root.render(<Header />)
